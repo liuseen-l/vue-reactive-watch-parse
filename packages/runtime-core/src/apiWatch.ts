a6901e88e58c02ef514d5402a056e25979270962
Mr.Liu <773890440@qq.com>
2023-01-09 07:20:37 +0800

    update the watch

--- packages/runtime-core/src/apiWatch.ts
+++ packages/runtime-core/src/apiWatch.ts
@@ -251,8 +251,6 @@ function doWatch(
   // 以上的代码都是为收集依赖做铺垫
 
 
-
-  
   let cleanup: () => void
   let onCleanup: OnCleanup = (fn: () => void) => {
     cleanup = effect.onStop = () => {
@@ -302,8 +300,15 @@ function doWatch(
   job.allowRecurse = !!cb
 
   let scheduler: EffectScheduler
+  /**
+    直接执行 job 函数，这本质上相当于 'sync' 的实现机制，即同步执
+    行。对于 options.flush 的值为 'pre' 的情况，我们暂时还没有办
+    法模拟，因为这涉及组件的更新时机，其中 'pre' 和 'post' 原本的
+    语义指的就是组件更新前和更新后 
+    sync立即执行 -> pre更新之前 -> post更新之后，默认现在是 pre
+  */
   if (flush === 'sync') {
-    scheduler = job as any // the scheduler function gets called directly
+    scheduler = job as any // 立即执行
   }
   // flush 本质上是在指定调度函数的执行时机，当 flush 的值为 'post' 时，代表调度函数需要将副作用函数放到一个微任务队列中，并等待 DOM 更新结束后再执行
   else if (flush === 'post') {
