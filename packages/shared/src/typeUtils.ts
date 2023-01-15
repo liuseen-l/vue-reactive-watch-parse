f6d322e81d741be97ae3cac95489c41572837170
code-ManL <773890440@qq.com>
2023-01-16 01:24:50 +0800

    fix:effect parse

--- packages/shared/src/typeUtils.ts
+++ packages/shared/src/typeUtils.ts
@@ -8,5 +8,4 @@ export type UnionToIntersection<U> = (
 export type LooseRequired<T> = { [P in keyof (T & Required<T>)]: T[P] }
 
 // If the type T accepts type "any", output type Y, otherwise output type N.
-// https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
 export type IfAny<T, Y, N> = 0 extends 1 & T ? Y : N
